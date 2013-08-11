8c8d2a790e16c08a7464f77904b51cfd7a3d1673
randName <kwokshungit@gmail.com>
2013-08-11 16:06:33 +0800

    redirect to setup if first run

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php header("Location: omxplayer.php"); ?>
+<?php
+if ( ! file_exists('omx_php.sh') ) {
+	// send to setup
+	header("Location: setup.php");
+} else {
+	header("Location: omxplayer.php");
+}
+?>
